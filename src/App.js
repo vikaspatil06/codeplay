f24e87c3a36ba6b7601c22dc42942114f128c559
vikaspatil06 <patil.vikas6306@gmail.com>
2025-05-20 21:37:38 +0530

    Setup React app and TicTacToe placeholder component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to CodePlay 🎮</h1>
+      <TicTacToe />
     </div>
   );
 }
---
+++ src/components/TicTacToe/TicTacToe.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TicTacToe() {
+  return <h2>Tic Tac Toe Game coming soon...</h2>;
+}
